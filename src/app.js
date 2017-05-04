ec697d351193584bf4d3d3b1ebd7b2eac71a9ecf
sonam.w.bhutia <sonam.w.bhutia@accenture.com>
2017-05-04 13:47:05 +0100

    latest

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@
 var express = require("express");
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.use(express.static("webapp"));
-app.listen(8080, function(){
-	console.log("listening on port 8080");
+app.listen(port, function(){
+	console.log("listening on port " + port);
 });
