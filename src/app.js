1eae24b451d37029c3217d03021d6c63fd956ac7
sbhutia <sbhutia13@gmail.com>
2017-05-04 13:22:27 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ var express = require("express");
 var app = express();
 
 app.use(express.static("webapp"));
-app.listen(8080, function(){
+app.listen(80, function(){
 	console.log("listening on port 8080");
 });
